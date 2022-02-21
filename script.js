dfa4995d811550e76ade3cfb9d59ca9d69b15a49
Gustavo Torchelsen <gustavotorchelsen@gmail.com>
2022-02-21 20:43:53 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,5 @@
+//var sexo = document.getElementById('');
+var peso = document.getElementById('peso');
+var altura = document.getElementById('altura');
+var idade = document.getElementById('idade');
+
